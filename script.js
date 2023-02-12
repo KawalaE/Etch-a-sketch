f9c505a47a0609edd109754b840eb9e85a596c46
Edyta Kawala <edyta.wer.kawala@gmail.com>
2023-02-12 21:55:21 +0100

    Create 16x16 grid pattern

---
+++ script.js
@@ -0,0 +1,18 @@
+let grid = document.querySelector('.grid');
+let verticalArray = [];
+let horizontalArray = [];
+
+let numOfSquares = 16;
+
+for (let i = 0; i < numOfSquares; i++){
+    verticalArray[i] = document.createElement('div');
+    grid.appendChild(verticalArray[i]);
+
+    for(let j = 0; j < numOfSquares; j++){
+        horizontalArray[j] = document.createElement('div');
+        horizontalArray[j].classList.add('square');
+        verticalArray[i].appendChild(horizontalArray[j]);
+    }
+
+}
+
